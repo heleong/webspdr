fba6c335b0e93fa865d0b008ecb2466d3fd22e28
Zhonglei He <LeongHe@Zhongleis-MacBook.local>
2018-01-02 15:00:13 +0800

    modify to adapt to python3

--- re01.py
+++ re01.py
@@ -4,13 +4,13 @@ import re
 
 pattern = re.compile(r'hello')
 
-print 'Pattern is: ' + str(pattern) 
+print('Pattern is: ' + str(pattern)) 
 
 m = ''
 while(m != 'exit'):
-    m = str(raw_input('Input match string: '))
+    m = str(input('Input match string (type exit to quit): '))
     if pattern.match(m):
-        print pattern.match(m).group()
-        print pattern.match(m).groups()
+        print(pattern.match(m).group())
+        print(pattern.match(m).groups())
     else:
-        print 'Match failed!'
+        print('Match failed! (Try again)')
